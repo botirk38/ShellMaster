4f182faee9785188265992d3db9116899e0746ad
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-06-30 22:49:52 +0100

    Implement REPL functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  while (true) {
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";
+    std::cout << "$ ";
+
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found\n";
+  }
 }
